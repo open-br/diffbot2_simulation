f89ba62177d386f3b0449983d73a85245410212b
ivan1993br <ivan1993br@github.com>
2020-12-20 14:03:06 +0000

    :bug: Correcting usage of substitutions

--- launch/diffbot2.launch.py
+++ launch/diffbot2.launch.py
@@ -14,22 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-from pathlib import Path
-
-import launch
-from launch import LaunchContext, LaunchDescription
+from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, PathJoinSubstitution
 from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-import xacro
 
 
 def generate_launch_description():
-    context = LaunchContext()
-
     launch_params = LaunchDescription()
 
     launch_params.add_action(
@@ -55,12 +49,6 @@ def generate_launch_description():
     pkg_diffbot2_desc_path_sub = FindPackageShare('diffbot2_description')
     pkg_diffbot2_simulation_path_sub = FindPackageShare('diffbot2_simulation')
     pkg_ros_ign_gazebo_path_sub = FindPackageShare('ros_ign_gazebo')
-    pkg_diffbot2_desc_share_prefix = Path(
-        pkg_diffbot2_desc_path_sub.perform(context))
-    pkg_ros_ign_gazebo_share_prefix = Path(
-        pkg_ros_ign_gazebo_path_sub.perform(context))
-    pkg_diffbot2_simulation_share_prefix = Path(
-        pkg_diffbot2_simulation_path_sub.perform(context))
 
     should_launch_server_sub = PythonExpression([" ' -s ' ", ' if ',
                                                 LaunchConfiguration('server_only'), ' else ',
@@ -68,25 +56,29 @@ def generate_launch_description():
 
     ign_gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            str(
-                pkg_ros_ign_gazebo_share_prefix / Path('launch') / Path(
-                    'ign_gazebo.launch.py'))),
+            PathJoinSubstitution(
+                [pkg_ros_ign_gazebo_path_sub, 'launch',
+                 'ign_gazebo.launch.py'])),
         launch_arguments={
-            'ign_args': ['-r ', should_launch_server_sub, str(
-                pkg_diffbot2_simulation_share_prefix / os.path.join(
-                    'worlds', 'diffbot2_default.sdf'))]
+            'ign_args': ['-r ', should_launch_server_sub, PathJoinSubstitution(
+                [pkg_diffbot2_simulation_path_sub,
+                 'worlds', 'diffbot2_default.sdf'])]
         }.items())
 
     include_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(
-        str(pkg_diffbot2_desc_share_prefix / os.path.join(
-            'launch', 'spawn_robot.launch.py'))),
+        PathJoinSubstitution([pkg_diffbot2_desc_path_sub,
+                              'launch', 'spawn_robot.launch.py'])),
         launch_arguments={'namespace': LaunchConfiguration(
             'namespace')}.items())
 
     # There is no parameter server, each node has its own parameters
-    xacro_file = str(pkg_diffbot2_desc_share_prefix / os.path.join(
-        'urdf', 'diffbot2.xacro'))
-    robot_description = xacro.process(xacro_file)
+
+    xacro_command_sub = Command([
+                                'xacro ',
+                                PathJoinSubstitution([
+                                            pkg_diffbot2_desc_path_sub,
+                                            'urdf', 'diffbot2.xacro']),
+                                ])
 
     bridge = Node(
         package='ros_ign_bridge',
@@ -113,7 +105,7 @@ def generate_launch_description():
         package='ros_ign_gazebo',
         executable='create',
         arguments=['-world', 'default',
-                   '-string', robot_description,
+                   '-string', xacro_command_sub,
                    '-name', LaunchConfiguration('robot_name'),
                    '-allow_renaming', 'true',
                    '-z', '1'],
@@ -121,7 +113,7 @@ def generate_launch_description():
         namespace=LaunchConfiguration('namespace')
     )
 
-    return launch.LaunchDescription([
+    return LaunchDescription([
         launch_params,
         include_launch,
         bridge,
