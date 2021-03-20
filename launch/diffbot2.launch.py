a5ff4b68509abb81e8d06ed0f073941884515571
Caio Amaral <caioaamaral@gmail.com>
2021-03-20 19:08:23 -0300

    Improve launch file structure

--- launch/diffbot2.launch.py
+++ launch/diffbot2.launch.py
@@ -17,106 +17,100 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, PathJoinSubstitution
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import PythonExpression
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    launch_params = LaunchDescription()
-
-    launch_params.add_action(
-        DeclareLaunchArgument(
-            name='namespace', default_value='diffbot2',
-            description='Node namespace'
-        )
-    )
-    launch_params.add_action(
-        DeclareLaunchArgument(
-            name='robot_name', default_value=LaunchConfiguration('namespace'),
-            description='Robot Name'
-        )
-    )
-
-    launch_params.add_action(
-        DeclareLaunchArgument(
-            name='server_only', default_value='False',
-            description='No gui, only server'
-        )
-    )
-
-    pkg_diffbot2_desc_path_sub = FindPackageShare('diffbot2_description')
-    pkg_diffbot2_simulation_path_sub = FindPackageShare('diffbot2_simulation')
-    pkg_ros_ign_gazebo_path_sub = FindPackageShare('ros_ign_gazebo')
-
-    should_launch_server_sub = PythonExpression([" ' -s ' ", ' if ',
-                                                LaunchConfiguration('server_only'), ' else ',
-                                                " ' ' "])
-
-    ign_gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution(
-                [pkg_ros_ign_gazebo_path_sub, 'launch',
-                 'ign_gazebo.launch.py'])),
-        launch_arguments={
-            'ign_args': ['-r ', should_launch_server_sub, PathJoinSubstitution(
-                [pkg_diffbot2_simulation_path_sub,
-                 'worlds', 'diffbot2_default.sdf'])]
-        }.items())
-
-    include_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(
-        PathJoinSubstitution([pkg_diffbot2_desc_path_sub,
-                              'launch', 'spawn_robot.launch.py'])),
-        launch_arguments={'namespace': LaunchConfiguration(
-            'namespace')}.items())
-
-    # There is no parameter server, each node has its own parameters
-
-    xacro_command_sub = Command([
-                                'xacro ',
-                                PathJoinSubstitution([
-                                            pkg_diffbot2_desc_path_sub,
-                                            'urdf', 'diffbot2.xacro']),
-                                ])
-
-    bridge = Node(
-        package='ros_ign_bridge',
-        executable='parameter_bridge',
-        arguments=[['/model/', LaunchConfiguration('namespace'),
-                    '/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist'],
-                   ['/model/', LaunchConfiguration('namespace'),
-                    '/odometry@nav_msgs/msg/Odometry@ignition.msgs.Odometry']],
-        output='screen',
-        remappings=[
-                (['/model/', LaunchConfiguration('namespace'),
-                  '/cmd_vel'],
-                 ['/', LaunchConfiguration('namespace'),
-                  '/cmd_vel']),
-                (['/model/', LaunchConfiguration('namespace'),
-                  '/odometry'],
-                 ['/', LaunchConfiguration('namespace'),
-                  '/odometry'])
-                ],
-        namespace=LaunchConfiguration('namespace')
-    )
-
-    spawn_robot = Node(
-        package='ros_ign_gazebo',
-        executable='create',
-        arguments=['-world', 'default',
-                   '-string', xacro_command_sub,
-                   '-name', LaunchConfiguration('robot_name'),
-                   '-allow_renaming', 'true',
-                   '-z', '1'],
-        output='screen',
-        namespace=LaunchConfiguration('namespace')
-    )
-
     return LaunchDescription([
-        launch_params,
-        include_launch,
-        bridge,
-        ign_gazebo,
-        spawn_robot
-        ])
+        # Arguments
+        DeclareLaunchArgument(
+            name='namespace',
+            default_value='diffbot2',
+            description='Node namespace'
+        ),
+        DeclareLaunchArgument(
+            name='robot_name',
+            default_value=LaunchConfiguration('namespace'),
+            description='Robot Name'
+        ),
+        DeclareLaunchArgument(
+            name='server_only',
+            default_value='false',
+            description='No gui, only server'
+        ),
+
+        # ign gazebo
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                __path([FindPackageShare('ros_ign_gazebo'), 'launch', 'ign_gazebo.launch.py'])
+            ),
+            launch_arguments={
+                'ign_args': [
+                    '-r ',
+                    PythonExpression([
+                        '"-s " if "true" == "', LaunchConfiguration('server_only'),
+                        '" else ""'
+                    ]),
+                    __path([
+                        FindPackageShare('diffbot2_simulation'), 'worlds', 'diffbot2_default.sdf'
+                    ])
+                ]
+            }.items()
+        ),
+
+        # diffbot2_description
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                __path([
+                    FindPackageShare('diffbot2_description'), 'launch', 'spawn_robot.launch.py'
+                ])
+            ),
+            launch_arguments={'namespace': LaunchConfiguration('namespace')}.items()
+        ),
+
+        # ros ign bridge
+        Node(
+            package='ros_ign_bridge',
+            namespace=LaunchConfiguration('namespace'),
+            executable='parameter_bridge',
+            arguments=[
+                ['/model/', LaunchConfiguration('namespace'),
+                 '/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist'],
+
+                ['/model/', LaunchConfiguration('namespace'),
+                 '/odometry@nav_msgs/msg/Odometry@ignition.msgs.Odometry']
+            ],
+            output='screen',
+            remappings=[
+                (['/model/', LaunchConfiguration('namespace'), '/cmd_vel'],
+                 ['/', LaunchConfiguration('namespace'), '/cmd_vel']),
+
+                (['/model/', LaunchConfiguration('namespace'), '/odometry'],
+                 ['/', LaunchConfiguration('namespace'), '/odometry'])]
+        ),
+
+        # robot spawn
+        Node(
+            package='ros_ign_gazebo',
+            namespace=LaunchConfiguration('namespace'),
+            executable='create',
+            arguments=[
+                '-world', 'default', '-string',
+                __xacro_load([FindPackageShare('diffbot2_description'), 'urdf', 'diffbot2.xacro']),
+                '-name',
+                LaunchConfiguration('robot_name'), '-allow_renaming', 'true', '-z', '1'
+            ],
+            output='screen'
+        ),
+    ])
+
+
+def __path(subst_array):
+    return PathJoinSubstitution(subst_array)
+
+
+def __xacro_load(xacro_path):
+    return Command(['xacro ', __path(xacro_path)])
